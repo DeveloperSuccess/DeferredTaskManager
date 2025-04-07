633b2eb25c667a7ae9a90e8b4cc0d1276d701f4a
Arthur Bogdanov <megafreez@gmail.com>
2025-04-07 10:08:34 +0500

    Возможность регистрировать кастомные обработчики через DI

--- DeferredTaskManager/DependencyInjection.cs
+++ DeferredTaskManager/DependencyInjection.cs
@@ -1,21 +1,62 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 
 namespace DTM
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddDeferredTaskManager<T>(this IServiceCollection services, Action<DeferredTaskManagerOptions<T>> configureOptions)
+        public static IServiceCollection AddDeferredTaskManager<T>(
+            this IServiceCollection services,
+            Action<DeferredTaskManagerOptions<T>> configureOptions,
+            Type? pubSubType = null,
+            Type? eventStorageType = null,
+            Type? eventSenderType = null,
+            Type? deferredTaskManagerServiceType = null)
         {
             services.Configure(configureOptions);
 
-            services.AddSingleton<IPoolPubSub, PoolPubSub>();
+            AddSingleton<IPoolPubSub, PoolPubSub>(pubSubType);
 
-            services.AddSingleton<IEventStorage<T>, EventStorageDefault<T>>();
+            AddSingleton<IEventStorage<T>, EventStorageDefault<T>>(eventStorageType);
 
-            services.AddSingleton<IEventSender<T>, EventSenderDefault<T>>();
+            AddSingleton<IEventSender<T>, EventSenderDefault<T>>(eventSenderType);
 
-            services.AddSingleton<IDeferredTaskManagerService<T>, DeferredTaskManagerService<T>>();
+            AddSingleton<IDeferredTaskManagerService<T>, DeferredTaskManagerService<T>>(deferredTaskManagerServiceType);
+
+            void AddSingleton<TServiceType, TDefaultType>(Type? customType)
+            {
+                services.AddSingleton(typeof(TServiceType), serviceProvider =>
+                {
+                    if (customType != null)
+                        return Activator.CreateInstance(customType, GetConstructorArguments(serviceProvider, customType));
+
+                    return Activator.CreateInstance(typeof(TDefaultType), GetConstructorArguments(serviceProvider, typeof(TDefaultType)));
+                });
+            }
+
+            object[] GetConstructorArguments(IServiceProvider serviceProvider, Type type)
+            {
+                var constructor = type.GetConstructors()
+                    .OrderByDescending(c => c.GetParameters().Length)
+                    .FirstOrDefault();
+
+                if (constructor == null)
+                {
+                    throw new InvalidOperationException($"No public constructor found for type {type.FullName}.");
+                }
+
+                var parameters = constructor.GetParameters();
+                object[] arguments = new object[parameters.Length];
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    var parameterType = parameters[i].ParameterType;
+                    arguments[i] = serviceProvider.GetRequiredService(parameterType);
+                }
+
+                return arguments;
+            }
 
             return services;
         }
